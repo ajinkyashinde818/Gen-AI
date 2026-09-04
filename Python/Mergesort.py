a0bbeaa7f665e864aa6c95975b2d5586dfaa310e
Ajinkya Shinde <ajinkyashinde818@gmail.com>
2026-09-04 22:41:42 +0530

    Exception Handling

--- Python/Mergesort.py
+++ Python/Mergesort.py
@@ -7,38 +7,33 @@ def merge_sort(arr):
         left = arr[:mid]
         right = arr[mid:]
 
-        # Recursion
         merge_sort(left)
         merge_sort(right)
 
         i = 0
         j = 0
 
-        # Merge using for loop
         for k in range(len(arr)):
 
-            # If left array is finished
             if i >= len(left):
                 arr[k] = right[j]
-                j += 1
+                j =j+1
 
-            # If right array is finished
             elif j >= len(right):
                 arr[k] = left[i]
-                i += 1
+                i =i+1
 
-            # Compare both elements
             elif left[i] < right[j]:
                 arr[k] = left[i]
-                i += 1
+                i=i+1
 
             else:
                 arr[k] = right[j]
-                j += 1
+                j=j+1
 
 
-arr = [8, 3, 5, 1]
+ticket_prices = [5000, 2000,2000, 8000, 3000,6589,6534,8657]
 
-merge_sort(arr)
+merge_sort(ticket_prices)
 
-print(arr)
+print(ticket_prices)
