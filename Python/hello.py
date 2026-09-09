c843bce5e1397253817e14c4f8577f43b13a85cf
Ajinkya Shinde <ajinkyashinde818@gmail.com>
2026-09-09 18:11:26 +0530

    Polymorphisam

--- Python/hello.py
+++ Python/hello.py
@@ -1,1 +1,6 @@
-print("Hello Dadus")
+def append_to_lst(val, lst=[]):  
+    lst.append(val)  
+    return lst 
+    print(append_to_lst(1)) 
+    print(append_to_lst(2)) 
+    print(append_to_lst(3, [])) 
